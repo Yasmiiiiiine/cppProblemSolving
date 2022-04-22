e4bd7cc5a51a554496ceb0fbd0009a88de4934ff
Yasmine Ali <67972643+Yasmiiiiiine@users.noreply.github.com>
2022-04-22 06:32:16 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+int number1 , number2 ;
+cin >> number1 >>number2 ;
+
+
+cout<<number1%10 + number2%10;
+
+
+    return 0;
+}
+
+
+
+
+
+//478712416751519618 906380052706837826
