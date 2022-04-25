6c83bff58cf777a9c1cf410530c7202f47063486
Yasmine Ali <67972643+Yasmiiiiiine@users.noreply.github.com>
2022-04-25 10:59:24 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
-int main(){
-    long long int a,b,c,d,x;
-    cin >>a>>b>>c>>d;
-    x = (a*b)-(c*d);
-    cout << "Difference = "<<x;
-
-    return 0;
+#define ll long long
+int
+main ()
+{
+  int age, x, y, z;
+  cin >> age;
+  cout << age / 365 << " years\n";
+  x = age % 365;
+  age = x;
+  y = age % 30;
+  age = y;
+  cout << x / 30 << " months\n";
+  cout << y << " days\n";
+  return 0;
 }
