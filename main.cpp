d69a65eda730a51797a8dc793c41353a0abe7c8d
Yasmine Ali <67972643+Yasmiiiiiine@users.noreply.github.com>
2022-04-25 11:05:25 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,14 +4,19 @@ using namespace std;
 int
 main ()
 {
-  int age, x, y, z;
-  cin >> age;
-  cout << age / 365 << " years\n";
-  x = age % 365;
-  age = x;
-  y = age % 30;
-  age = y;
-  cout << x / 30 << " months\n";
-  cout << y << " days\n";
-  return 0;
+  int number;
+    cout << "Enter a number : " << endl;
+    cin >> number;
+
+
+
+    while(number >= 10){
+        number = number/10;
+    }
+
+    if(number % 2 == 0)
+        cout<<"EVEN";
+    else
+        cout<<"ODD";
+    return 0;
 }
